652f311d2d59eff919e425fe0f1e9ee0ffc2fcd2
Bulat Shakirzyanov <bulat@twilio.com>
2013-07-16 23:35:19 -0700

    remove unncessary module

--- lib/rdaux/web.rb
+++ lib/rdaux/web.rb
@@ -1,7 +1,3 @@
-module RDaux
-  module Web
-    require 'rdaux/web/application'
-    require 'rdaux/web/server'
-    require 'rdaux/web/site'
-  end
-end
+require 'rdaux/web/application'
+require 'rdaux/web/server'
+require 'rdaux/web/site'
